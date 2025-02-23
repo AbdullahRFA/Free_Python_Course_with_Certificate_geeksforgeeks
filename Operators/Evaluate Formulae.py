f2a18f37fb09c74a536f17b35a5e3154b4b4fe55
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-23 13:45:10 +0600

    Evalute formula

---
+++ Operators/Evaluate Formulae.py
@@ -0,0 +1,38 @@
+"""
+Given four inputs that are stored in variables a, b, c, and d. You need to write an expression to evaluate the following formula. Use integer division. The expression should be a single statement.
+
+
+
+Examples:
+
+Input: a = 10, b = 4, c = 7, d = 9
+Output: 11
+Explanation: 10 + 4 = 14, 14 // 7 = 2(Python) or 14 / 7 = 2(Java or CPP or C), 2 + 9 = 11.
+
+"""
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+
+########### Write your code below ###############
+#Complete the code below that stores the output of the expression
+sm = a+b
+div = sm//c
+
+res = div+d
+########### Write your code above ###############
+
+#This prints the output. Don't change it!
+print(res)
+
+#{
+ # Driver Code Starts.
+
+# } Driver Code Ends
