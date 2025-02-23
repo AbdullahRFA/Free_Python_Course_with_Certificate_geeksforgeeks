9e69f67c77ae12c3f49f102ac139eb75eeb55764
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-23 13:50:16 +0600

    Sum of N numbers

---
+++ Operators/Sum of N Numbers.py
@@ -0,0 +1,33 @@
+"""
+Given an integer n find the sum of the first n natural number.
+
+Examples:
+
+Input: n = 10
+Output: 55
+Explanation: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55.
+Input: n = 5
+Output: 15
+Explanation: 1 + 2 + 3 + 4 + 5 = 15.
+"""
+
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+n = int(input())
+
+########### Write your code below ###############
+# Compute the sum of first n natural numbers
+ans = n*(n+1)//2
+########### Write your code above ###############
+
+print(ans)
+
+
+#{
+ # Driver Code Starts.
+
+# } Driver Code Ends
