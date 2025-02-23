5b4fa11ba44246ba004ab29c5d32bce89428dfbf
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-23 13:52:07 +0600

    Last digit of a number

---
+++ Operators/Last Digit of a number.py
@@ -0,0 +1,32 @@
+"""
+Given an integer n. Write a program to print the last digit of the number. Also, move the cursor to the next line.
+
+Examples:
+
+Input: n = 10
+Output: 0
+Input: n = 9768
+Output: 8
+"""
+
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+n = int(input())
+
+########### Write your code below ###############
+# Find last digit of given number n
+ans = abs(n)%10
+########### Write your code above ###############
+
+# Print ans
+print(ans)
+
+
+#{
+ # Driver Code Starts.
+
+# } Driver Code Ends
