ae5b91971864f5585eb3cdcfa6a22747a7cb398d
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-25 14:06:03 +0600

    Even Odd game

---
+++ Flow_Control/Even Odd Game.py
@@ -0,0 +1,30 @@
+"""
+Given a number n, number of apples in a bag. You and your friend are picking one apple turnwise from the bag. It is given that the first attempt is always by you. The person picking the last apple will be the winner.
+
+If you will win: print "You" (without quotes)
+If your friend will win: print "Friend" (without quotes)
+Examples:
+
+Input: n = 9
+Output: You
+Input: n = 4
+Output: Friend
+"""
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+# Take n input and print who wins
+num = int(input())
+if num%2 ==0:
+    print("Friend")
+else:
+    print("You")
+
+#{
+ # Driver Code Starts.
+
+# } Driver Code Ends
