dbf2b24229cd291ad5debe60f625b1ebcccbad22
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-26 10:51:32 +0600

    calculator

---
+++ Flow_Control/Calculator.py
@@ -0,0 +1,27 @@
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+# Take a, b and operator input and print the output
+a = int(input())
+b = int(input())
+c = int(input())
+if c not in (1, 2, 3):
+    print("Invalid Input")
+
+elif c == 1:
+    print(a + b)
+elif c == 2:
+    print(b - a)
+else:
+    print(a * b)
+# Your code here
+
+
+# {
+# Driver Code Starts.
+
+# } Driver Code Ends
