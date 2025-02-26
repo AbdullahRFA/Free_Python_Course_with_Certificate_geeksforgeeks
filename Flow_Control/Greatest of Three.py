96edc6d0b3b20ec387193d7d28bbf5847f1e7bcd
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-26 10:37:46 +0600

    max amoung threee

---
+++ Flow_Control/Greatest of Three.py
@@ -0,0 +1,29 @@
+"""
+Given three numbers a, b, and c. You need to find which is the greatest of them all.
+
+Examples:
+
+Input: a = 1, b = 2, c = 3
+Output: 3
+Explanation: Clearly, c = 3 is the greatest of (1, 2, 3)
+Input: a = 2, b = 2, c = 5
+Output: 5
+Explanation: Out of (2, 2, 5) 5 is the greatest
+"""
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+# Take a, b and c input and print the greatest of three
+a = int(input())
+b= int(input())
+c= int(input())
+print(max(a,b,c))
+
+#{
+ # Driver Code Starts.
+
+# } Driver Code Ends
