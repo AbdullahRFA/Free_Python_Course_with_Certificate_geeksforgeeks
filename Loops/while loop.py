0f4114de11faa00693a50766a589c9b0c3cdf5fc
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-03 23:41:19 +0600

    while loop

---
+++ Loops/while loop.py
@@ -0,0 +1,22 @@
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+x = int(input())
+
+# Your code here
+# for i in range(x,-1,-1):
+#     print(i,end=' ')
+
+while x >=0:
+    print(x,end=' ')
+    x -= 1
+
+
+#{
+ # Driver Code Starts.
+
+# } Driver Code Ends
