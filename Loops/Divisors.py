ede0ff96837b28061dad5c6b9ea776cb6d1cb21f
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-05 10:29:27 +0600

    Divisors

---
+++ Loops/Divisors.py
@@ -0,0 +1,18 @@
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+n = int(input())
+
+# Your code here
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i, end=' ')
+
+# {
+# Driver Code Starts.
+
+# } Driver Code Ends
