d6665ed6f784329260e0334877bbb09d7df006eb
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-06 19:32:26 +0600

    GCD

---
+++ Loops/GCD.py
@@ -0,0 +1,32 @@
+"""
+Given two numbers a and b. The task is to find the GCD of  a and b.
+The GCD of two numbers is the largest number that can divide both a and b perfectly.
+
+Examples:
+
+Input: a = 6, b = 9
+Output: 3
+Explanation: After 3 there is no number that can divide both 6 and 9 perfectly.
+Input: a = 10, b = 15
+Output: 5
+Explanation: 5 is the greatest common divisor of 10 and 15.
+"""
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+import math
+a = int(input())
+b = int(input())
+
+# Your code here
+print(math.gcd(a,b))
+
+
+#{
+ # Driver Code Starts.
+
+# } Driver Code Ends
