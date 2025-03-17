13893431303d2f3af131b088051513576e8dec94
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-17 16:21:51 +0600

    Prime Factorizations

---
+++ function/Prime Factorization.py
@@ -0,0 +1,43 @@
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+
+
+# } Driver Code Ends
+
+#User function Template for python3
+class Solution:
+    def is_prime(self, n):
+        for i in range(2, int(n**0.5)+1):
+            if n%i==0:
+                return False
+        return True
+    def printPrimeFactorization(self, n):
+        # Your code here
+        for i in range(2,n+1):
+            if self.is_prime(i):
+                while n%i==0:
+                    print(i,end=' ')
+                    n//=i
+
+#{
+ # Driver Code Starts.
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        N = int(input())
+
+        ob = Solution()
+        ob.printPrimeFactorization(N)
+        print()
+        print('~')
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
