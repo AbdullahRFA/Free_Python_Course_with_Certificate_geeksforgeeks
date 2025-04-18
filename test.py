76dd51cfd70727b371a9f6d7a78e6194866d2922
AbdullahRFA <shakibrybmn@gmail.com>
2025-04-18 16:01:02 +0600

    added mehtod overriding

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-class bb:
-    count =0
-    @classmethod
-    def incre(cls):
-        cls.count += 1
 
-bb.incre()
-print(bb.count)
+n = int(input())
+for i in range(n):
+    C_name = input()
+    NameList = [x[0] for x in C_name.split()]
+    print("".join(NameList))
